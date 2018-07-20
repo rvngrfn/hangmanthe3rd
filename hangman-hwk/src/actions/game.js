3da13d9c3b6500734d0d9aa59d519218b8a9c14a
rvngrfn <rroberson@wesleyan.edu>
2018-07-20 22:43:16 +0200

    homework base

---
+++ hangman-hwk/src/actions/game.js
@@ -0,0 +1,3 @@
+export const NEW_HELLO_WORLD = 'NEW_HELLO_WORLD'
+export const NEW_GAME = 'NEW_GAME'
+export const MAKE_GUESS = 'MAKE_GUESS'
---
+++ hangman-hwk/src/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import ReduxThunk from 'redux-thunk'
+
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducer, enhancer)
+
+export default store
